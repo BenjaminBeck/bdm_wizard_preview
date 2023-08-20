d04703dafbb1d9b967c5baa41a5624c8f951182c
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2023-08-20 22:10:36 +0200

    refactore to use CType as filenames

--- README.md
+++ README.md
@@ -1,23 +1,19 @@
-
 ## TYPO3 Extension: Wizard-Preview
 
 
-### Before
+### Initial Version
 ![wizzard-before.png](Documentation%2Fwizzard-before.png)
 
 
-### After
+### Enhanced Version
 ![wizzard-after.png](Documentation%2Fwizzard-after.png)
-
-
-
 
 ### Compatibility
 ATM its only testet with TYPO3 11.5
 
 ### Installation
 
-1. Add repository to composer.json
+1. Start by adding our repository to your composer.json:
    ```yaml
    "repositories": [
      {
@@ -26,15 +22,17 @@ ATM its only testet with TYPO3 11.5
      }
    ],
    ```
-2. Install via composer
+2. Install the extension using composer:
    ```sh
    composer req bdm/bdm_wizard_preview:^11.5
    ```
 
-3. Set extension configuration to match your folder structure
+3. Adjust the extension configuration to align with your folder structure.
    ![extension-settings.png](Documentation%2Fextension-settings.png)
 
-4. Place the preview images in the configured folder. The filenames must match the identifier of the content-icon inside the new content wizard. As a help the required filename is shown in the wizard if TYPO3 is in development context and no image has been found.
+4. Adding Preview Images<br>
+   Place the preview images in the specified folder. Ensure filenames correspond with the CType. In TYPO3 development context, if images are absent, the wizard will guide you by displaying the required filename.
+    
    ![extension-filename-help.png](Documentation%2Fextension-filename-help.png)
 
 
--- Resources/Public/JavaScript/Backend.js
+++ Resources/Public/JavaScript/Backend.js
@@ -27,6 +27,13 @@ define(
 		const fileExistsAsPreviewImage = function (filename){
 			return allPreviewImageFilenamesArray.includes(filename);
 		};
+		const getCTypeFromButton = function ($button){
+			const target = $button.data('target');
+			const params = getAllUrlParams(target);
+			const CType = params['defvals%5btt_content%5d%5bctype%5d'];
+			return CType;
+		};
+
 		const checkInterval = setInterval(function () {
 			wizardItemSelector = wizardItemSelector + ':not(.modded)';
 			const $els = $('body').find(wizardItemSelector);
@@ -37,8 +44,11 @@ define(
 					$el.addClass('modded');
 					const data = $el.data();
 					if(data){
-						const filename = data.identifier+ '.png';
 						const $container = $el.closest('.t3js-media-new-content-element-wizard');
+						const $button = $container.find('button');
+						const CType = getCTypeFromButton($button);
+						const filenameWoExtension = CType;
+						const filename = filenameWoExtension+ '.png';
 						const loadAndAppendPreviewImage = function (filename, isVariant = false){
 							const $dfdLoaded = $.Deferred();
 							let src = imageRootPath;
@@ -61,12 +71,12 @@ define(
 						if(fileExistsAsPreviewImage(filename)) {
 							const $promiseLoaded = loadAndAppendPreviewImage(filename);
 							let variantNumber = 1;
-							let variantFilename = data.identifier+ '-variant-' + variantNumber + '.png';
+							let variantFilename = filenameWoExtension+ '-variant-' + variantNumber + '.png';
 							$.when($promiseLoaded).then(function (){
 								while(fileExistsAsPreviewImage(variantFilename)) {
 									const $promiseLoaded = loadAndAppendPreviewImage(variantFilename, true);
 									variantNumber++;
-									variantFilename = data.identifier+ '-variant-' + variantNumber + '.png';
+									variantFilename = filenameWoExtension+ '-variant-' + variantNumber + '.png';
 								}
 							});
 						} else {
@@ -87,7 +97,6 @@ define(
 								$container.append($input);
 							}
 						}
-						const $button = $container.find('button.btn.btn-link');
 						$container.on('click', function (e){
 							// prevent infinite loop: if the button is clicked, don't click it again
 							if($(e.target).is($button)){
@@ -100,5 +109,72 @@ define(
 				});
 			}
 		}, 100);
+
+		function getAllUrlParams(url) {
+
+			// get query string from url (optional) or window
+			var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+			// we'll store the parameters here
+			var obj = {};
+
+			// if query string exists
+			if (queryString) {
+
+				// stuff after # is not part of query string, so get rid of it
+				queryString = queryString.split('#')[0];
+
+				// split our query string into its component parts
+				var arr = queryString.split('&');
+
+				for (var i = 0; i < arr.length; i++) {
+					// separate the keys and the values
+					var a = arr[i].split('=');
+
+					// set parameter name and value (use 'true' if empty)
+					var paramName = a[0];
+					var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+					// (optional) keep case consistent
+					paramName = paramName.toLowerCase();
+					if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+					// if the paramName ends with square brackets, e.g. colors[] or colors[2]
+					if (paramName.match(/\[(\d+)?\]$/)) {
+
+						// create key if it doesn't exist
+						var key = paramName.replace(/\[(\d+)?\]/, '');
+						if (!obj[key]) obj[key] = [];
+
+						// if it's an indexed array e.g. colors[2]
+						if (paramName.match(/\[\d+\]$/)) {
+							// get the index value and add the entry at the appropriate position
+							var index = /\[(\d+)\]/.exec(paramName)[1];
+							obj[key][index] = paramValue;
+						} else {
+							// otherwise add the value to the end of the array
+							obj[key].push(paramValue);
+						}
+					} else {
+						// we're dealing with a string
+						if (!obj[paramName]) {
+							// if it doesn't exist, create property
+							obj[paramName] = paramValue;
+						} else if (obj[paramName] && typeof obj[paramName] === 'string'){
+							// if property does exist and it's a string, convert it to an array
+							obj[paramName] = [obj[paramName]];
+							obj[paramName].push(paramValue);
+						} else {
+							// otherwise add the property
+							obj[paramName].push(paramValue);
+						}
+					}
+				}
+			}
+
+			return obj;
+		}
+
+
 	return {};
 });
