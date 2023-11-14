1bcb056d5a6d1dcb1bdfb6e6f0c4a63a843166f6
Benjamin Beck <benjamin@Benjamins-MacBook-Pro.local>
2023-11-14 20:33:43 +0100

    update

--- Classes/Backend/PreRender.php
+++ Classes/Backend/PreRender.php
@@ -14,42 +14,95 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 final class PreRender
 {
     public function addRequireJsConfiguration($a, PageRenderer $pageRenderer){
-        if (($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface
-             && ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
-         ) {
 
-            if(isset($_GET['id'])){
-                $pageUid = $_GET['id'];
-                $pageTsConfig = BackendUtility::getPagesTSconfig($pageUid);
-                if(
-                    isset($pageTsConfig['bdm_wizard_preview.'])
-                    && isset($pageTsConfig['bdm_wizard_preview.']['previewImagePath'])
-                ){
-                    $previewImagePath = $pageTsConfig['bdm_wizard_preview.']['previewImagePath'];
-                }
-                if(empty($previewImagePath)){
-                    return;
-                }
-                $pageRenderer->loadRequireJsModule('TYPO3/CMS/BdmWizardPreview/Backend');
-                $extensionKey = 'bdm_wizard_preview';
-                $configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get($extensionKey);
-            }else{
-                $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
-                return;
-            }
-            $absoluteImagePath = GeneralUtility::getFileAbsFileName($previewImagePath);
-            $allPreviewImages = GeneralUtility::getAllFilesAndFoldersInPath([], $absoluteImagePath, 'png', false, 2, '');
-            $absoluteImagePath = \TYPO3\CMS\Core\Utility\PathUtility::getPublicResourceWebPath ($previewImagePath);
-            $configuration['previewImagePath'] = $absoluteImagePath;
-            $configuration['allPreviewImagePaths'] = $allPreviewImages;
-            $applicationContext = Environment::getContext();
-            $isDevelepmentContext = $applicationContext->isDevelopment();
-            $configuration['isDevelepmentContext'] = $isDevelepmentContext;
-            $pageRenderer->addJsInlineCode(
-                'bdm_wizard_preview_extension_config',
-                'var bdm_wizard_preview_extension_config = ' . json_encode($configuration) . ';'
-            );
+
+        $isValidRequest = ($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface;
+        if(!$isValidRequest) return;
+
+        $isBackendApplicationType = ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend();
+        if(!$isBackendApplicationType) return;
+
+        $requestHasGetId = isset($_GET['id']);
+        if(!$requestHasGetId) {
             $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
+            return;
         }
+
+        $pageUid = (int)$_GET['id'];
+        $pageTsConfig = BackendUtility::getPagesTSconfig($pageUid);
+        $previewImagePath = $pageTsConfig['bdm_wizard_preview.']['previewImagePath'] ?? '';
+        $hasPreviewImagePathFromPageTs = !empty($previewImagePath);
+        $hasPreviewImagePathFromExtensionConfiguration = false;
+        if(!$hasPreviewImagePathFromPageTs) {
+            $extensionConfiguration = GeneralUtility::makeInstance(ExtensionConfiguration::class);
+            $previewImagePath = $extensionConfiguration->get('bdm_wizard_preview', 'previewImagePath');
+            $hasPreviewImagePathFromExtensionConfiguration = !empty($previewImagePath);
+        }
+        if(!$hasPreviewImagePathFromPageTs && !$hasPreviewImagePathFromExtensionConfiguration) {
+            $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
+            $pageRenderer->addJsInlineCode('bdm_wizard_preview_extension_config', 'console.warn("bdm_wizard_preview: No previewImagePath found in pageTsConfig or extension configuration");');
+            return;
+        }
+
+        $absoluteFilesystemImagePath = GeneralUtility::getFileAbsFileName($previewImagePath);
+        $allPreviewImages = GeneralUtility::getAllFilesAndFoldersInPath([], $absoluteFilesystemImagePath, 'png', false, 2, '');
+        $absoluteUrlImagePath = \TYPO3\CMS\Core\Utility\PathUtility::getPublicResourceWebPath ($previewImagePath);
+        $hasAnyPreviewImages = !empty($allPreviewImages);
+        if(!$hasAnyPreviewImages) {
+//            $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
+            $pageRenderer->addJsInlineCode('bdm_wizard_preview_extension_config_alert', 'console.warn("bdm_wizard_preview: No preview images found in ' . $absoluteFilesystemImagePath . '");');
+//            return;
+        }
+        $pageRenderer->loadRequireJsModule('TYPO3/CMS/BdmWizardPreview/Backend');
+        $extensionKey = 'bdm_wizard_preview';
+//        $configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get($extensionKey);
+        $configuration = [];
+        $configuration['previewImagePath'] = $absoluteUrlImagePath;
+        $configuration['allPreviewImagePaths'] = $allPreviewImages;
+        $applicationContext = Environment::getContext();
+        $isDevelepmentContext = $applicationContext->isDevelopment();
+        $configuration['isDevelepmentContext'] = $isDevelepmentContext;
+        $pageRenderer->addJsInlineCode(
+            'bdm_wizard_preview_extension_config',
+            'var bdm_wizard_preview_extension_config = ' . json_encode($configuration) . ';'
+        );
+        $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
+
+
+
+//        if (($GLOBALS['TYPO3_REQUEST'] ?? null) instanceof ServerRequestInterface
+//             && ApplicationType::fromRequest($GLOBALS['TYPO3_REQUEST'])->isBackend()
+//         ) {
+//
+//            if(isset($_GET['id'])){
+//                $pageUid = $_GET['id'];
+//                $pageTsConfig = BackendUtility::getPagesTSconfig($pageUid);
+//
+//
+//
+//
+//
+//
+//                $pageRenderer->loadRequireJsModule('TYPO3/CMS/BdmWizardPreview/Backend');
+//                $extensionKey = 'bdm_wizard_preview';
+//                $configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get($extensionKey);
+//            }else{
+////                $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
+////                return;
+//            }
+//            $absoluteImagePath = GeneralUtility::getFileAbsFileName($previewImagePath);
+//            $allPreviewImages = GeneralUtility::getAllFilesAndFoldersInPath([], $absoluteImagePath, 'png', false, 2, '');
+//            $absoluteImagePath = \TYPO3\CMS\Core\Utility\PathUtility::getPublicResourceWebPath ($previewImagePath);
+//            $configuration['previewImagePath'] = $absoluteImagePath;
+//            $configuration['allPreviewImagePaths'] = $allPreviewImages;
+//            $applicationContext = Environment::getContext();
+//            $isDevelepmentContext = $applicationContext->isDevelopment();
+//            $configuration['isDevelepmentContext'] = $isDevelepmentContext;
+//            $pageRenderer->addJsInlineCode(
+//                'bdm_wizard_preview_extension_config',
+//                'var bdm_wizard_preview_extension_config = ' . json_encode($configuration) . ';'
+//            );
+//            $pageRenderer->addCssFile('EXT:bdm_wizard_preview/Resources/Public/Styles/backend-wizard.css');
+//        }
     }
 }
